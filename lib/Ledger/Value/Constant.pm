fe99ebe3436cff55f79f2713aeadc1d0e7fce333
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2016-01-25 18:07:23 +0100

    Add a 'Constant' Value type
    It will be used for automatic line build with contant text.

---
+++ lib/Ledger/Value/Constant.pm
@@ -0,0 +1,25 @@
+package Ledger::Value::Constant;
+use Moose;
+use namespace::sweep;
+
+extends 'Ledger::Value::Str';
+
+around 'value' => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    return $self->$orig()
+	unless @_;
+
+    my $msg = shift;
+    $msg =~ s/^\s+//;
+    $msg =~ s/\s+$//;
+    if ($msg ne $self->default_value) {
+	die Ledger::Exception::ValueParseError->new(
+	    'message' => "Invalid value '$msg' for constant '".$self->default_value."'",
+	    );
+    }
+    return $self->$orig($msg);
+};
+
+1;
