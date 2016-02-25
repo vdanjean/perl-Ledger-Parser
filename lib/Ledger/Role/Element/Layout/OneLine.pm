b2ee671279a4d54f06bff588501c73a52bbf8f0a
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2016-02-25 16:32:42 +0100

    A one-line element can also be iterated
    This is mostly usefull to iterate on its values

--- lib/Ledger/Role/Element/Layout/OneLine.pm
+++ lib/Ledger/Role/Element/Layout/OneLine.pm
@@ -4,6 +4,13 @@ use namespace::sweep;
 
 with (
     'Ledger::Role::Element::Layout::Base',
+    # allows to iterate on values
+    'Ledger::Role::Iterator::Elements' => {
+	-alias  => { 
+	    'getElementsIterator' => 'iterator',
+	    'getValuesElementsIterator' => 'valuesIterator',
+	},
+    },
     );
 
 sub load_from_reader {
@@ -15,6 +22,11 @@ sub load_from_reader {
 sub as_string {
     my $self=shift;
     return $self->gettext;
+}
+
+sub _iterable_elements {
+    my $self = shift;
+    return ();
 }
 
 1;
